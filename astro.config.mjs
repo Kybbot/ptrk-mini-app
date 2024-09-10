7f7b60c2fcb4bf3f2ea0cef3083d071bf0090c0e
Kybbot <frontendtakepart@gmail.com>
2024-09-10 09:31:13 +0300

    Small updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
+
 
 // https://astro.build/config
 export default defineConfig({
 	output: "hybrid",
-	adapter: vercel(),
+	adapter: cloudflare(),
 	i18n: {
 		defaultLocale: "ru",
 		locales: ["ru", "ua"],
